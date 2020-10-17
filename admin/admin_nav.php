7a47fe0dfa4ea661cbd9969a380a2a3ec1bbd0bf
fatihohn <fatihohn@gmail.com>
2020-10-17 22:34:08 +0900

    new article

--- admin/admin_nav.php
+++ admin/admin_nav.php
@@ -2,14 +2,20 @@
             <ul class="nav_group">
                 <li class="nav_item">
                     <mark>.flat tables</mark>
-                    <a href="/admin_list">
+                    <a href="/admin_list.php">
                         평상들
                     </a>
                 </li>
                 <li class="nav_item">
                     <mark>.about us</mark>
-                    <a href="/admin_about">
+                    <a href="/admin_about.php">
                         평상으로부터
+                    </a>
+                </li>
+                <li class="nav_item">
+                    <mark>.about us</mark>
+                    <a href="/admin_new_article.php">
+                        새로운 평상+
                     </a>
                 </li>
                 <!-- @yield ('nav_item') -->
