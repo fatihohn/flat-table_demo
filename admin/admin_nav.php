57eefb939421400c79013ce91481f3b4387c9ed9
fatihohn <fatihohn@gmail.com>
2020-10-17 22:36:04 +0900

    new article

--- admin/admin_nav.php
+++ admin/admin_nav.php
@@ -2,19 +2,19 @@
             <ul class="nav_group">
                 <li class="nav_item">
                     <mark>.flat tables</mark>
-                    <a href="/admin_list.php">
+                    <a href="admin_list.php">
                         평상들
                     </a>
                 </li>
                 <li class="nav_item">
                     <mark>.about us</mark>
-                    <a href="/admin_about.php">
+                    <a href="admin_about.php">
                         평상으로부터
                     </a>
                 </li>
                 <li class="nav_item">
                     <mark>.new flat table</mark>
-                    <a href="/admin_new_article.php">
+                    <a href="admin_new_article.php">
                         새로운 평상+
                     </a>
                 </li>
