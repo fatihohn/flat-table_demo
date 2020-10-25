596d5ca698d0f445f32f0f69ba01921bf3fd5feb
fatihohn <fatihohn@gmail.com>
2020-10-25 10:11:30 +0900

    css structure

--- admin/admin_head.php
+++ admin/admin_head.php
@@ -8,7 +8,7 @@
 
 <!-- Styles -->
 <link rel="stylesheet" href="../static/css/admin_important.css" type="text/css" media="all" />
-<!-- <link rel="stylesheet" href="../static/css/layout.css" type="text/css" media="all" /> -->
+<link rel="stylesheet" href="../static/css/layout.css" type="text/css" media="all" />
 <link rel="stylesheet" href="../static/css/admin.css" type="text/css" media="all" />
 <link rel="stylesheet" type="text/css" href="/trix-master/dist/trix.css">
     <script type="text/javascript" src="/trix-master/dist/trix.js"></script>
