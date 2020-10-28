e8edbff99ed7387bcadf73b1b363fa91bcc1fd30
fatihohn <fatihohn@gmail.com>
2020-10-28 22:26:43 +0900

    about header logo

--- admin/about.php
+++ admin/about.php
@@ -38,7 +38,7 @@
     <!-- @yield ('content') -->
     <section class="article_main about_main">
         <header class="container_header articles_header">
-            <img src="../static/img/flat_table_icon.svg" alt="flat_table_icon">
+            <img src="/static/img/flat_table_icon.svg" alt="flat_table_icon">
             <h1>평상으로부터</h1>
         </header>
         <div class="main">
