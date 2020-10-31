bce6ec2dd0c5735fefe8896783c7ca41fcfe3ca7
fatihohn <fatihohn@gmail.com>
2020-10-31 15:47:34 +0900

    img compress rate

--- admin/new_article.php
+++ admin/new_article.php
@@ -491,7 +491,7 @@
                     var imgSource = document.querySelector('#image_to_compress');
                     var canvas = document.getElementById("canvas");
                         console.log(file.size/1000000 + "MB");
-                        percentage = 74/(file.size/100000000 + 100);
+                        percentage = 80/(file.size/80000000 + 100);
                         console.log(percentage*100);
                     reader.addEventListener("load", function () {
                         console.log("loading image");
