dca2b20f2e6038247557a2062c194c83896fc8f4
fatihohn <fatihohn@gmail.com>
2020-10-31 17:59:34 +0900

    icr

--- admin/new_article.php
+++ admin/new_article.php
@@ -526,7 +526,8 @@
                                     } else if(imgRes/canvasRes > 1.5 && imgRes/canvasRes <= 2.2) {
                                         percentage = ((Math.pow(imgRes/canvasRes, 2))*20)/(file.size/100000000 + 100);
                                     } else {
-                                        percentage = 95/(file.size/100000000 + 100);
+                                        // percentage = ((Math.pow(canvasRes/imgRes, 2))*20)/(file.size/100000000 + 100);
+                                        percentage = 95/(file.size/1000000 + 100);
                                     }
                                     // percentage = 75/(file.size/5000000 + 100);
                                     // percentage = 95/(file.size/500000 + 100);
