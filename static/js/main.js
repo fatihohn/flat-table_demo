669fdc47ced87efe2bf608f2ab795a3b3c4ac569
fatihohn <fatihohn@gmail.com>
2020-11-01 11:40:43 +0900

    intro title

--- static/js/main.js
+++ static/js/main.js
@@ -232,9 +232,9 @@ function setIntroImg() {
             introTitle.classList.add(imgSrc.alt);
             introEnter.classList.add(imgSrc.alt);
             introTitle.innerHTML = imgSrc.title;
-            introTitleHeader.classList.add("active");
-            // setTimeout(function() {
-            // }, 600);
+            setTimeout(function() {
+                introTitleHeader.classList.add("active");
+            }, 600);
         }
 
         function hideIntroTitle() {
