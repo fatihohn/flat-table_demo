a82a7d1fea1fcbaafb941fd3c2040c55e57f5878
fatihohn <fatihohn@gmail.com>
2020-11-01 21:20:16 +0900

    tag func

--- admin/new_article_action.php
+++ admin/new_article_action.php
@@ -132,7 +132,8 @@
 
                         // $sql_old_tag_relation = "INSERT INTO articles_tags_map (article_id, tag_id) VALUES ('{$new_article_id}', '{$old_tag_id}')";//태그 맵 DB에 old 태그와 article의 id값을 저장한다
                         // $sql_old_tag_relation = "INSERT INTO articles_tags_map SET tag_fk = $old_tag_id, article_fk = LAST_INSERT_ID()";
-                        $sql_old_tag_relation = "INSERT INTO articles_tags_map SET tag_fk = (SELECT id FROM tags WHERE tag_name = $tag_input), article_fk = LAST_INSERT_ID()";
+                        // $sql_old_tag_relation = "INSERT INTO articles_tags_map SET tag_fk = (SELECT id FROM tags WHERE tag_name = $tag_input), article_fk = LAST_INSERT_ID()";
+                        $sql_old_tag_relation = "INSERT INTO articles_tags_map (tag_fk, article_fk) VALUES ((SELECT id FROM tags WHERE tag_name = $tag_input LIMIT 1), LAST_INSERT_ID())";
                         $result_old_tag_relation = mysqli_query($conn, $sql_old_tag_relation);
                         
                         
@@ -159,7 +160,8 @@
                                 // $sql_new_tag_relation = "INSERT INTO articles_tags_map (article_id, tag_id) VALUES ('{$new_article_id}', '{$new_tag_id}')";//태그 맵 DB에 new 태그와 article의 id값을 저장한다
                                 // $sql_new_tag_relation = "INSERT INTO articles_tags_map SET tag_fk = $new_tag_id, article_fk = LAST_INSERT_ID()";
                                 // $sql_old_tag_relation = "INSERT INTO articles_tags_map SET tag_fk = (SELECT id FROM tags WHERE tag_name = $tag_input), article_fk = LAST_INSERT_ID()";
-                                $sql_old_tag_relation = "INSERT INTO articles_tags_map SET tag_fk = LAST_INSERT_ID(), article_fk = LAST_INSERT_ID()";
+                                // $sql_old_tag_relation = "INSERT INTO articles_tags_map SET tag_fk = LAST_INSERT_ID(), article_fk = LAST_INSERT_ID()";
+                                $sql_old_tag_relation = "INSERT INTO articles_tags_map (tag_fk, article_fk) VALUES (LAST_INSERT_ID(), LAST_INSERT_ID())";
                             $result_new_tag_relation = mysqli_query($conn, $sql_new_tag_relation);
 
 
