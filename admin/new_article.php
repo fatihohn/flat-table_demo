052612cd433b480be38b4f9c1a1022b1ba99ea46
fatihohn <fatihohn@gmail.com>
2020-11-06 22:35:41 +0900

    hashtag func

--- admin/new_article.php
+++ admin/new_article.php
@@ -493,8 +493,8 @@
                             if(!tagFinder.querySelector(".selected")) {
                                 tagFinder.firstChild.classList.add("selected");
                                 // tagFinder.firstElementChild.classList.add("selected");
-                            // } else if(tagFinder.querySelector(".selected") && tagFinder.querySelector(".selected").nextSibling) {
-                            } else if(tagFinder.querySelector(".selected") && tagFinder.querySelector(".selected").nextElementSibling) {
+                            } else if(tagFinder.querySelector(".selected") && tagFinder.querySelector(".selected").nextSibling) {
+                            // } else if(tagFinder.querySelector(".selected") && tagFinder.querySelector(".selected").nextElementSibling) {
                                 tagFinder.querySelector(".selected").nextSibling.classList.add("selected");
                                 // tagFinder.querySelector(".selected").nextElementSibling.classList.add("selected");
                                 tagFinder.querySelector(".selected").classList.remove("selected");
@@ -505,8 +505,8 @@
                         } else if(e.keycode == 38 || e.code == "ArrowUp") {
                             e.preventDefault();
                             if(!tagFinder.querySelector(".selected")) {
-                                // tagFinder.lastChild.classList.add("selected");
-                                tagFinder.lastElementChild.classList.add("selected");
+                                tagFinder.lastChild.classList.add("selected");
+                                // tagFinder.lastElementChild.classList.add("selected");
                             } else if(tagFinder.querySelector(".selected") && tagFinder.querySelector(".selected").previousSibling) {
                             // } else if(tagFinder.querySelector(".selected") && tagFinder.querySelector(".selected").previousElementSibling) {
                                 tagFinder.querySelector(".selected").previousSibling.classList.add("selected");
