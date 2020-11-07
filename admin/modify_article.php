4de4c510a03dbbf7b27f73b168cc4220291ac81c
fatihohn <fatihohn@gmail.com>
2020-11-07 16:34:08 +0900

    tag container

--- admin/modify_article.php
+++ admin/modify_article.php
@@ -73,7 +73,6 @@
                     <input id="title" type="text" name="title" placeholder="제목" value="<?=$title?>" required/>
                     <div class="article_info">
                         <p class="article_address">
-                            <!-- {{-- 경기도 동두천시 상봉암동 153-15 --}} -->
                             <input id="address" type="text" name="address" placeholder="주소" value="<?=$address?>" required />
                         </p>
                         <p class="category">
@@ -81,8 +80,8 @@
                                 <div class="tag_list_wrap"></div>
                                 <div class="tag_input_wrap">
                                     <div class="tag_input">
-                                        <input id="tags" type="text" placeholder="연관태그를 입력해주세요. (최대 5개)" value="">
-                                        <input id="tag_vault" type="hidden" name="tag_vault" value="">
+                                        <input id="tags" type="text" placeholder="연관태그를 입력해주세요. (최대 5개)" value="" />
+                                        <input id="tag_vault" type="hidden" name="tag_vault" value="" />
                                     </div>
                                 </div>
                             </div>
