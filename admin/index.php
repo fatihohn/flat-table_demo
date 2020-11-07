0bc6a8ade3f7095c05d29f8e249c0237fd8c99d3
fatihohn <fatihohn@gmail.com>
2020-11-08 01:12:47 +0900

    tag sreach

--- admin/index.php
+++ admin/index.php
@@ -49,7 +49,7 @@
         $sql_article_data_all = "SELECT * FROM articles WHERE about!= 'on'";
             ?>
             <script>
-                console.log("<?=$article_with_hashtag?>");
+                console.log("<?=count($article_with_hashtag)?>");
             </script>
             <?php
         if(count($article_with_hashtag) > 0) {
