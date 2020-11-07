ef5d0cded3c2f9c3eae257314f9972b3a80c34fd
fatihohn <fatihohn@gmail.com>
2020-11-07 17:03:24 +0900

    update article tag

--- admin/modify_article_action.php
+++ admin/modify_article_action.php
@@ -9,8 +9,8 @@
     $table_address = $_POST['address'];
     $table_address = mysqli_real_escape_string($conn, $table_address);
 
-    $categories = $_POST['categories'];
-    $categories = mysqli_real_escape_string($conn, $categories);
+    // $categories = $_POST['categories'];
+    // $categories = mysqli_real_escape_string($conn, $categories);
 
     $imgs = $_POST['imgs'];
     $imgs = mysqli_real_escape_string($conn, $imgs);
@@ -33,17 +33,37 @@
     $about = $_POST['about'];
     $about = mysqli_real_escape_string($conn, $about);
 
+
+    $tag_vault = $_POST['tag_vault'];
+    $tag_vault = mysqli_real_escape_string($conn, $tag_vault);
+
+    $tag_vault = explode(",", $tag_vault);
+
+    // $new_tags = array();
+    $old_tags = array();
+    $old_tags_id_list = array();
+    $new_tags_id_list = array();
+
+    $sql_old_tags = "SELECT tag_name FROM tags";
+    $result_old_tags = mysqli_query($conn, $sql_old_tags);
+    while($rows_old_tags = $result_old_tags->fetch_assoc()) {
+        // array_push($old_tags, array($rows_old_tags['tag_name'], $rows_old_tags['id']));
+        array_push($old_tags, $rows_old_tags['tag_name']);
+        // array_push($old_tags_id_list, $rows_old_tags['id']);
+    }
+
+
     $sql = 
         "UPDATE articles SET
             `title` = '$title',
             `table_address` = '$table_address',
-            `categories` = '$categories',
             `imgs` = '$imgs',
             `comment` = '$comment',
             `content` = '$content',
             `photographer` = '$photographer',
             `words` = '$words',
             `flag` = '$flag',
+            `fieldwork_date` = '$fieldwork_date',
             `about` = '$about'
         WHERE `id` = '$q'";
         //     (username, title, table_address, categories, imgs, comment, content, photographer, words, flag, about)
@@ -69,5 +89,48 @@
         error_log(mysqli_error($conn));
     }
     else{
-        echo("<script>alert('평상이 수정되었습니다.');location.href='article.php?q=$q';</script>");
+        if(count($tag_vault) > 0) {//태그 입력이 있다면
+
+            for ($i = 0; $i < count($tag_vault); $i++) {
+                if (!in_array($tag_vault[$i], $item_tag_list)) {//기존에 맵핑된 태그가 아닌 경우
+                    if (in_array($tag_vault[$i], $old_tags)) {//전에 입력됐던 태그가 있다면
+                        ?>
+                        <script>
+                            console.log("old: "+"<?=$tag_vault[$i]?>")
+                        </script>
+                        <?php
+                        $sql_old_tag_relation = "INSERT INTO article_tag_map SET 
+                            tag_id = (SELECT id FROM tags WHERE tag_name = '$tag_vault[$i]'), 
+                            article_id = (SELECT MAX(id) FROM articles)";//태그 테이블 맵핑
+                        $result_old_tag_relation = mysqli_query($conn, $sql_old_tag_relation);
+                    
+                    } else {//새로운 태그라면
+                        $sql_new_tag_vault[$i] = "INSERT INTO tags (tag_name) VALUES ('{$tag_vault[$i]}')";//새로운 태그 저장
+                        $result_new_tag_vault[$i] = mysqli_query($conn, $sql_new_tag_vault[$i]);
+                        
+                        if ($result_new_tag_vault[$i]) {
+                            ?>
+                            <script>
+                                console.log("new: "+"<?=$tag_vault[$i]?>")
+                            </script>
+                            <?php
+                            $sql_new_tag_relation = "INSERT INTO article_tag_map SET 
+                                tag_id = (SELECT MAX(id) FROM tags), 
+                                article_id = (SELECT MAX(id) FROM articles)";//태그 테이블 맵핑
+                            $result_new_tag_relation = mysqli_query($conn, $sql_new_tag_relation);
+                        }
+                    }
+                }
+            }
+            echo("<script>alert('평상이 수정되었습니다.');location.href='article.php?q=$q';</script>");
+        } else {
+            echo("<script>alert('평상이 수정되었습니다.');location.href='article.php?q=$q';</script>");
+        }
+
+
+
+
+
+
+
     }
