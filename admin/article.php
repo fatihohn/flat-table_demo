ccbf7d52ad77eb8b3165bbb88815e099f42b128d
fatihohn <fatihohn@gmail.com>
2020-11-08 14:04:46 +0900

    fieldwork date

--- admin/article.php
+++ admin/article.php
@@ -38,7 +38,7 @@
     $photographer = $rows_article["photographer"];
     $words = $rows_article["words"];
     $fieldwork_date = $rows_article["fieldwork_date"];
-    $fieldwork_date = $fieldwork_date->format('Y/m/d');
+    $fieldwork_date->format('Y/m/d');
 
     // // $sql_article_data_flag = $sql_article_data_all." WHERE flag = flag";
     // $sql_article_data_flag = "SELECT * FROM articles WHERE flag = flag";
