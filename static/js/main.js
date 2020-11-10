fcf09211a94a3e7e020fccc8a640d2192466a836
fatihohn <fatihohn@gmail.com>
2020-11-10 22:42:59 +0900

    share link

--- static/js/main.js
+++ static/js/main.js
@@ -297,6 +297,32 @@ showArticleWithTag = function(tag) {
 //end FRONT
 
 
+
+
+
+function shareBtnClick() {
+    // let shareBtn = document.getElementById("share_btn");
+    let shareBtn = document.querySelectorAll(".share_btn");
+    function shareThings() {
+        let urlBox = document.createElement('input');
+        let currentUrl = window.location.href;
+
+        document.body.appendChild(urlBox);
+        urlBox.value = currentUrl;
+        urlBox.select();
+        document.execCommand('copy');
+        document.body.removeChild(urlBox);
+        alert("링크가 복사되었습니다.")
+    }
+    let sb;
+    for(sb=0; sb < shareBtn.length; sb++) {
+        if(shareBtn) {
+            shareBtn[sb].addEventListener("click", shareThings);
+        }
+    }
+}
+shareBtnClick();
+
 // //ARTICLE
 // function organizePics() {
 //     let articleImgs = document.querySelectorAll(".article_pics figure");
