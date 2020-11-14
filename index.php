29f644ad0701bcc0a6a71c3a66aca9f4d6284909
fatihohn <fatihohn@gmail.com>
2020-11-15 01:06:39 +0900

    meta tag

--- index.php
+++ index.php
@@ -15,10 +15,11 @@
     // $rows_article_all = mysqli_fetch_assoc($result_article_data_all);
     if(isset($hashTag)) {
         $hashTag = mysqli_real_escape_string($conn, $hashTag);
-        $hashTagMeta = "%".$hashTag."%";
+        // $hashTagMeta = "%".$hashTag."%";
+        $hashTagMeta = $hashTag;
         // $sql_get_hashtag_id = "SELECT * FROM tags WHERE tag_name = '$hashTag' LIMIT 1";
         // $result_get_hashtag_id = mysqli_query($conn, $sql_get_hashtag_id);
-        $sql_get_hashtag_id = "SELECT * FROM tags WHERE tag_name LIKE ? LIMIT 1";
+        $sql_get_hashtag_id = "SELECT * FROM tags WHERE tag_name LIKE %?% LIMIT 1";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql_get_hashtag_id)) {
             echo "query error";
