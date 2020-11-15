4a5e33205992b4939f0a10911b2d1af2b1ae6ae7
fatihohn <fatihohn@gmail.com>
2020-11-15 13:38:37 +0900

    img display

--- article.php
+++ article.php
@@ -172,7 +172,7 @@
         let articleImgs = document.querySelectorAll(".article_pics figure");
         let mobileImgs = document.querySelector(".article_pics_mobile");
             for(let m=0; m < articleImgs.length; m++) {
-                articleImgs[m].style.display = "block";
+                // articleImgs[m].style.display = "block";
                 articleImgs[m].childNodes[1].style.width = "100%";
                 if(window.innerWidth > 1080) {
                     if(mobileImgs.childNodes.length > 0) {
