10df3a67428ee1607015c484d869903f8ec9fa07
fatihohn <fatihohn@gmail.com>
2020-11-15 21:16:20 +0900

    tag trim

--- static/js/main.js
+++ static/js/main.js
@@ -310,7 +310,7 @@ if (document.querySelector(".slide_title a")) {
 var showArticleWithTag;
 showArticleWithTag = function(tag) {
     if(tag.trim() !== "") {
-        location.href = "index.php?q=ok&tag=" + tag;
+        location.href = "index.php?q=ok&tag=" + tag.trim();
     }
 }
 
