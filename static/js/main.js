19bfb18cd2fca66e994c61fce2bd285beb31fbcd
fatihohn <fatihohn@gmail.com>
2020-11-20 22:07:07 +0900

    read article opaticy

--- static/js/main.js
+++ static/js/main.js
@@ -28,7 +28,8 @@ manageNav();
 function showReadArticle() {
     let articles = document.querySelectorAll(".article");
     for (let j = 0; j < articles.length; j++) {
-        articles[j].childNodes[1].childNodes[1].style.transition = "all 1.2s ease 1s";
+        articles[j].childNodes[1].childNodes[1].style.transition = "opacity 1s 1.5s ease, visible .7s, color .7s";
+        
         articles[j].onmouseover = function() {
             articles[j].childNodes[3].style.background = "rgb(240, 230, 210)";
             articles[j].childNodes[1].childNodes[1].style.opacity = "0.9";
