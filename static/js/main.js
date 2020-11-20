c05fea8af135cb6e3ab28e1fb2edcb34749dded4
fatihohn <fatihohn@gmail.com>
2020-11-20 22:00:05 +0900

    read article opacity

--- static/js/main.js
+++ static/js/main.js
@@ -28,9 +28,10 @@ manageNav();
 function showReadArticle() {
     let articles = document.querySelectorAll(".article");
     for (let j = 0; j < articles.length; j++) {
+        articles[j].childNodes[1].childNodes[1].style.transition = "all 300ms";
         articles[j].onmouseover = function() {
             articles[j].childNodes[3].style.background = "rgb(240, 230, 210)";
-            articles[j].childNodes[1].childNodes[1].style.opacity = "1";
+            articles[j].childNodes[1].childNodes[1].style.opacity = "0.9";
             articles[j].childNodes[1].childNodes[1].style.visibility = "visible";
             articles[j].childNodes[1].childNodes[1].style.display = "initial";
         }
