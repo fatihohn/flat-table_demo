e11b97cdc7fe1f18449e60ed37971632d5171897
fatihohn <fatihohn@gmail.com>
2020-11-20 23:05:00 +0900

    no file input

--- admin/new_article.php
+++ admin/new_article.php
@@ -1148,6 +1148,9 @@
         document.querySelector("trix-editor").onfocusout = function() {
             document.querySelector(".trix-button.trix-button--icon.trix-button--icon-attach").disabled = true;
         }
+        document.querySelector("trix-editor").oninput = function() {
+            document.querySelector(".trix-button.trix-button--icon.trix-button--icon-attach").disabled = true;
+        }
     </script>
     <script> //hashtag related
        (function () {
