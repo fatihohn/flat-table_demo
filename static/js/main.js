710f904023b1a162aada485e5126eb0f6743ff62
fatihohn <fatihohn@gmail.com>
2020-11-21 16:42:22 +0900

    show intro title fast

--- static/js/main.js
+++ static/js/main.js
@@ -174,7 +174,8 @@ function setIntroImg() {
         }
         introSlide.style.backgroundImage = "url('" + slideImgSrc[0].src + "')";
         slideImgSrc[0].onload = function() {
-            showIntroTitleFast(slideImgSrc[0]);
+            // showIntroTitleFast(slideImgSrc[0]);
+            showIntroTitle(slideImgSrc[0]);
         }
         prevBtn.onclick = function() {
             if (prevBtn.classList.item(2)) {
@@ -256,12 +257,11 @@ function setIntroImg() {
         function showIntroTitleFast(imgSrc) {
             // imgSrc.onload = function() {
             // if (imgSrc.alt !== "") {
-            setTimeout(function() {
-                addIntroTitleClass(imgSrc, function() {
-                    addIntroTitleActive(imgSrc)
-                });
-
-            }, 300);
+            // setTimeout(function() {
+            addIntroTitleClass(imgSrc, function() {
+                addIntroTitleActive(imgSrc);
+            });
+            // }, 300);
             // }
             // }
         }
